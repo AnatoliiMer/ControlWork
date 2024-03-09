85a02a14a51fe5ea236e6ce7a8d9dd4c314d235d
Anatolii <tumerovo@yandex.ru>
2024-03-09 13:40:10 +0500

    added Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        string result = FilterArray(inputArray);
+
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static string FilterArray(string[] inputArray)
+    {
+        int count = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = "\"" + str + "\"";
+                index++;
+            }
+        }
+
+        return "[" + string.Join(", ", resultArray) + "]";
+    }
+}
